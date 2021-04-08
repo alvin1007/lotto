2e70775cf33efc405b61448f2457fec53cc95d76
Young Jea Cho <77112874+alvin1007@users.noreply.github.com>
2021-04-08 23:27:37 +0900

    Update lotto.c

--- lotto.c
+++ lotto.c
@@ -2,62 +2,163 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+void persent1(void)
 {
-	int i, random = 0;
-	while (1)
+	int lotto_num, random;
+	srand(time(NULL));
+	random = rand() % 2;
+	printf("\n첫번째 입력 (0 ~ 1) : ");
+	scanf("%d", &lotto_num);
+	if (lotto_num == random)
 	{
-		int usr_lotto[23] = { 0 }, lotto_num[23] = { 0 }, cnt_true = 0;
+		printf("정답 : %d\n\n", random);
+		printf("당첨입니다!\n\n\n\n");
+	}
+	else
+	{
+		printf("정답 : %d\n", random);
+		printf("아쉽게도 다른 숫자입니다.\n\n\n\n");
+	}
+}
+
+void persent2(void)
+{
+	int lotto_num, random, cnt = 0, random_arr[2];
+	for (int i = 0; i < 2; i++)
+	{
 		srand(time(NULL));
-		for (i = 0; i < 23; i++)
+		random = rand() % 2;
+		printf("%d번째 입력 (0 ~ 1) : ", i + 1);
+		scanf("%d", &lotto_num);
+		if (lotto_num == random)
 		{
-			random = rand() % 2;
-			lotto_num[i] = random;
+			cnt++;
 		}
-		for (i = 1; i < 24; i++)
+		random_arr[i] = random;
+	}
+	if (cnt == 2)
+	{
+		printf("정답 : ");
+		for (int i = 0; i < 2; i++)
 		{
-			printf("%d번째 숫자 입력 : ", i);
-			scanf("%d", &usr_lotto[i]);
-		}
-		for (i = 0; i < 23; i++)
-		{
-			if (usr_lotto[i] == lotto_num[i])
-			{
-				cnt_true++;
-			}
-		}
-		printf("\n");
-		printf("로또 번호 : ");
-		for (i = 0; i < 23; i++)
-		{
-			printf("%d", lotto_num[i]);
+			printf("%d ", random_arr[i]);
 		}
 		printf("\n\n");
-		if (cnt_true == 23)
+		printf("당첨입니다!\n\n\n\n");
+	}
+	else
+	{
+		printf("정답 : ");
+		for (int i = 0; i < 2; i++)
 		{
-			printf("1등입니다!");
+			printf("%d ", random_arr[i]);
 		}
-		else if (cnt_true < 23 && cnt_true >= 18)
+		printf("\n\n");
+		printf("아쉽지만 다른 숫자입니다.\n\n\n\n");
+	}
+}
+
+void persent3(void)
+{
+	int lotto_num, random, cnt = 0, random_arr[2];
+	for (int i = 0; i < 2; i++)
+	{
+		srand(time(NULL));
+		random = rand() % 10;
+		printf("%d번째 입력 (0 ~ 9) : ", i + 1);
+		scanf("%d", &lotto_num);
+		if (lotto_num == random)
 		{
-			printf("2등입니다!");
+			cnt++;
 		}
-		else if (cnt_true < 18 && cnt_true >= 13)
+		random_arr[i] = random;
+	}
+	if (cnt == 2)
+	{
+		printf("정답 : ");
+		for (int i = 0; i < 2; i++)
 		{
-			printf("3등입니다!");
+			printf("%d ", random_arr[i]);
 		}
-		else if (cnt_true < 13 && cnt_true >= 8)
+		printf("\n\n");
+		printf("당첨입니다!\n\n\n\n");
+	}
+	else
+	{
+		printf("정답 : ");
+		for (int i = 0; i < 2; i++)
 		{
-			printf("4등입니다!");
+			printf("%d ", random_arr[i]);
 		}
-		else if (cnt_true < 8 && cnt_true >= 3)
+		printf("\n\n");
+		printf("아쉽지만 다른 숫자입니다.\n\n\n\n");
+	}
+}
+
+void persent4(void)
+{
+	int lotto_num, random, cnt = 0, random_arr[4];
+	for (int i = 0; i < 4; i++)
+	{
+		srand(time(NULL));
+		random = rand() % 10;
+		printf("%d번째 입력 (0 ~ 9) : ", i + 1);
+		scanf("%d", &lotto_num);
+		if (lotto_num == random)
 		{
-			printf("5등입니다!");
+			cnt++;
 		}
-		else
+		random_arr[i] = random;
+	}
+	if (cnt == 4)
+	{
+		printf("정답 : ");
+		for (int i = 0; i < 4; i++)
 		{
-			printf("6등입니다!");
+			printf("%d ", random_arr[i]);
 		}
-		printf("\n\n\n\n");
+		printf("\n\n");
+		printf("당첨입니다!\n\n\n\n");
+	}
+	else
+	{
+		printf("정답 : ");
+		for (int i = 0; i < 4; i++)
+		{
+			printf("%d ", random_arr[i]);
+		}
+		printf("\n\n");
+		printf("아쉽지만 다른 숫자입니다.\n\n\n\n");
+	}
+}
+
+int main()
+{
+	int mode;
+	while (1)
+	{
+		printf("확률 선택\n\n50%% 모드(1)     25%% 모드(2)     1%% 모드(3)     0.01%% 모드(4)\n\n입력 : ");
+		scanf("%d", &mode);
+		if (mode == 1)
+		{
+			printf("\n");
+			persent1();
+		}
+		else if (mode == 2)
+		{
+			printf("\n");
+			persent2();
+		}
+		else if (mode == 3)
+		{
+			printf("\n");
+			persent3();
+		}
+		else if (mode == 4)
+		{
+			printf("\n");
+			persent4();
+		}
 	}
 	return 0;
 }
